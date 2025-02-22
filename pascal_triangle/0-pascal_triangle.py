3238c5758c0c3f3a44924a3f3f3574efe58c8b8c
Didier Nsengiyumva <nsedidier@gmail.com>
2025-02-22 22:02:46 +0200

    latest updates with pascal function

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """Returns a list of integer lists representing the pascArr triangle n"""
+    if n <= 0:
+        return [] 
+    pascArr = [[1]]  
+    for i in range(1, n): 
+        row = [1]
+        for j in range(1, i):
+            row.append(pascArr[i - 1][j - 1] + pascArr[i - 1][j])
+        row.append(1) 
+        pascArr.append(row) 
+    return pascArr 
---
+++ pascal_triangle/README.md
@@ -0,0 +1,2 @@
+Here's the documentation for the 0-pascal_triangle.py file
+
